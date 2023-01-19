da5c76d8fadbee6f037bc614be8ce59b038a6b1e
Sumit211995 <sumitcskaktwan@gmail.com>
2023-01-20 01:15:06 +0530

    Modify JavaScript and CSS files

--- Landing-page/script.js
+++ Landing-page/script.js
@@ -1,15 +1,22 @@
-//document.getElementById("form").addEventListener("click" , submitForm, false);
-
 function mainForm(){
-  var firstName=document.getElementById("firstname").value;
-  var lastName=document.getElementById("lastname").value;
-  var mbNo=document.getElementById("mbno").value;
-  var email=document.getElementById("email").value;
-  var message=document.getElementById("message").value;
+  var firstName = document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  var mbNo = document.getElementById("mbno").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
 
   var letters = /^[A-Za-z]+$/;  
-  var num= /[0-9]/;
-  var email_val= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;  
+  var num = /[0-9]/;
+  var email_val = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;  
+
+  const formReset = document.getElementById("form-submit");
+
+  this.editData(firstName,lastName,mbNo,email,message,letters,num,email_val,formReset);  
+ 
+}
+
+function editData(firstName,lastName,mbNo,email,message,letters,num,email_val,formReset) 
+{
 
   if(firstName==="" || lastName==="" || mbNo==="" || message===""){
     alert("Enter each Details Correctly");
@@ -30,11 +37,37 @@ function mainForm(){
     alert("please write the message atleast 10 character.");
   }
   else{
-    alert("Form Submit Successfully");
-  }
-  
+    // alert("Form Submit Successfully" +
+    //        alert("First Name: "+ firstName +
+    //        " Last Name: "+ lastName));
+      const displayDetails =document.getElementById("output1");
+      displayDetails.innerHTML = "First Name : "+firstName  + " <br> " +"Last Name : " +lastName + " <br> " + " Mobile No : "+mbNo +" <br>  "+ "Your Message : " +message;
+          this.Reset(formReset);
+      }
 }
-function submit() {
-  document.getElementById("form").reset();
-  
+
+function Reset(formReset) {
+  formReset.reset();
+}
+
+function toggleMenu() {
+
+  const toggleStyle = document.querySelector('.collapse');
+
+  if (toggleStyle.style.display === 'none'){
+
+    toggleStyle.style.display ='block';
+
+  } else 
+  {
+    toggleStyle.style.display = 'none';
+  }
+
+}
+function productDetails(){
+  alert("Comming Soon");
+}
+
+function contactUs(){
+  location.href="form.html";
 }
